055551655445b5b77caa4810c8539ac15df4588d
FruityOkapi <josephbsimmerman@gmail.com>
2022-10-18 10:53:47 -0400

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,14 @@
-// This makes a new empty object that has all the values for the current date and time
-var date = new Date();
 // These make new objects for day month and year that update with the current date.
-var day = date.getDay();
-var dayNum = date.getDate();
-var month = date.getMonth(); 
-var year = date.getFullYear();
-var currentHour = date.getHours();
+var currentHourMoment = moment().format('h');
+// parses the string from moment into a integer for proper math to occur later
+var currentHour = parseInt(currentHourMoment);
+// adds id selecting for certain elements to be appended
 var container = $('.container');
 var validateSave = $('#validated-save');
 // This will be to set the date on the title. So the var selects the p element by id and the code following will directly set the text to the current date
 var dateTitle = $('#currentDay');
 dateTitle.text(moment().format('ddd. MMM Do, YYYY'));
-// This next block of code is to make a new array with the date
+// This next block of code is to make a new array with the times in 24 hour format which will be fixed in the first function
 var times = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 // 
 var ids = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
@@ -19,10 +16,13 @@ var ids = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 var appointments = ['', '', '', '', '', '', '', '', ''];
 // This will be the function that creates the planner
 function createPlanner(){
+    // For loop creates each block and does the delegation on what is in the future and what is in the past and present while also setting functionality for buttons to save whats written in the correct textareas.
     for (var i = 0; i < times.length; i++) {
+        // makes new div and adds classes
         var timeblock = $('<div>');
         timeblock.addClass('time-block');
         timeblock.addClass('row');
+        // makes new div and adds classes with an if statement that sets the text of this div to am or PM
         var hour = $('<div>');
         hour.addClass('hour');
         if (times[i] > 12){
@@ -30,10 +30,12 @@ function createPlanner(){
         } else {
             hour.text(times[i] + ' AM');
         }
+        // makes new textarea. adds id corresponding to the current box being made. adds a row. adds text from appointments array.
         var formT = $('<textarea>');
         formT.attr('id', ids[i]);
         formT.attr('rows', '3');
         formT.text(appointments[i]);
+        // this else if statement checks current time and adds a class based on current time
         if (currentHour < times[i]) {
             formT.addClass('future');
         } else if (currentHour == times[i]) {
@@ -41,6 +43,7 @@ function createPlanner(){
         } else {
             formT.addClass('past');
         }
+        // makes a button, adds a class, and sets the function to save the text of the textarea that was made in the same iteration of the for loop as the button. There might be a better way to code this but the only way I could think of doesn't work in jQuery syntax. I try to get "effectedButton" to select an id in an array based on the value of 'i' with a for loop but because of the way jQuery syntax works it makes it impossible because of the quotes make it a string.
         var bttn = $('<button>');
         bttn.addClass('saveBtn');
         if (i === 0) {
@@ -107,23 +110,24 @@ function createPlanner(){
                 dispSaveNotif();
             })
         }
+        // makes a save icon and appends it to the button
         var saveIcon = $('<i>');
         saveIcon.addClass('bi');
         saveIcon.addClass('bi-save');
         bttn.append(saveIcon);
+        // appending the elements created above to the page
         timeblock.append(hour);
         timeblock.append(formT);
         timeblock.append(bttn);
         container.append(timeblock);
     }
 }
+// sets the array to the current planner
 function setAppointments() {
     var getAppts = localStorage.getItem('appointments');
     appointments = JSON.parse(getAppts);
 }
-function dispSaveNotif() {
-    validateSave.show().fadeOut(3000);
-}
+// this makes the save notif and makes it not visible
 function SaveNotif() {
     var saveNotif = $('<h2>');
     saveNotif.text('Saved appointment! ')
@@ -135,7 +139,11 @@ function SaveNotif() {
     validateSave.append(calIcon);
     validateSave.show().fadeOut(0);
 }
-// This is for first time launch
+// makes the save notif display for 3 seconds while fading out
+function dispSaveNotif() {
+    validateSave.show().fadeOut(3000);
+}
+// This is for first time launch so the item appointments in the localStorage doesn't get set as a null object if my code tries to grab an item that doesn't exist it sets it as null and is unable to be altered.
 var lsApp = localStorage.getItem('appointments');
 if (lsApp === null) {
     localStorage.setItem('appointments', JSON.stringify(appointments));
