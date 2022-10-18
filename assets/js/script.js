e12baed0ab7fe7b799d9829543f31642f86d2027
FruityOkapi <josephbsimmerman@gmail.com>
2022-10-18 10:08:00 -0400

    finished the save notif along with the rest of the functionality of the planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ var month = date.getMonth();
 var year = date.getFullYear();
 var currentHour = date.getHours();
 var container = $('.container');
+var validateSave = $('#validated-save');
 // This will be to set the date on the title. So the var selects the p element by id and the code following will directly set the text to the current date
 var dateTitle = $('#currentDay');
 dateTitle.text(moment().format('ddd. MMM Do, YYYY'));
@@ -15,7 +16,7 @@ var times = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
 // 
 var ids = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 // This is to set the appointment array.
-var appointments = ['eeee', '', '', '', '', '', '', '', ''];
+var appointments = ['', '', '', '', '', '', '', '', ''];
 // This will be the function that creates the planner
 function createPlanner(){
     for (var i = 0; i < times.length; i++) {
@@ -46,49 +47,63 @@ function createPlanner(){
                 var effectedButton = $('#9am');
                 appointments[0] = effectedButton.val();
                 localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 1) {
             bttn.click(function() {
                 var effectedButton = $('#10am');
                 appointments[1] = effectedButton.val();
                 localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 2) {
             bttn.click(function() {
                 var effectedButton = $('#11am');
                 appointments[2] = effectedButton.val();
                 localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 3) {
             bttn.click(function() {
                 var effectedButton = $('#12pm');
                 appointments[3] = effectedButton.val();
                localStorage.setItem('appointments', JSON.stringify(appointments));
+               dispSaveNotif();
             })
         } else if (i === 4) {
             bttn.click(function() {
                 var effectedButton = $('#1pm');
-                appointments[4] = effectedButton.val();                localStorage.setItem('appointments', JSON.stringify(appointments));
+                appointments[4] = effectedButton.val();                
+                localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 5) {
             bttn.click(function() {
                 var effectedButton = $('#2pm');
-                appointments[5] = effectedButton.val();                localStorage.setItem('appointments', JSON.stringify(appointments));
+                appointments[5] = effectedButton.val();                
+                localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 6) {
             bttn.click(function() {
                 var effectedButton = $('#3pm');
-                appointments[6] = effectedButton.val();                localStorage.setItem('appointments', JSON.stringify(appointments));
+                appointments[6] = effectedButton.val();                
+                localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 7) {
             bttn.click(function() {
                 var effectedButton = $('#4pm');
-                appointments[7] = effectedButton.val();                localStorage.setItem('appointments', JSON.stringify(appointments));
+                appointments[7] = effectedButton.val();                
+                localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         } else if (i === 8) {
             bttn.click(function() {
                 var effectedButton = $('#5pm');
-                appointments[8] = effectedButton.val();                localStorage.setItem('appointments', JSON.stringify(appointments));
+                appointments[8] = effectedButton.val();                
+                localStorage.setItem('appointments', JSON.stringify(appointments));
+                dispSaveNotif();
             })
         }
         var saveIcon = $('<i>');
@@ -105,12 +120,26 @@ function setAppointments() {
     var getAppts = localStorage.getItem('appointments');
     appointments = JSON.parse(getAppts);
 }
-
+function dispSaveNotif() {
+    validateSave.show().fadeOut(3000);
+}
+function SaveNotif() {
+    var saveNotif = $('<h2>');
+    saveNotif.text('Saved appointment! ')
+    var calIcon = $('<i>');
+    calIcon.addClass('bi');
+    calIcon.addClass('bi-calendar-check-fill');
+    calIcon.attr('id', 'align-center')
+    validateSave.append(saveNotif);
+    validateSave.append(calIcon);
+    validateSave.show().fadeOut(0);
+}
 // This is for first time launch
 var lsApp = localStorage.getItem('appointments');
 if (lsApp === null) {
     localStorage.setItem('appointments', JSON.stringify(appointments));
 }
-// This is for displaying the page
+// This is for displaying the page and setting the correct appointments.
+SaveNotif();
 setAppointments();
 createPlanner();
